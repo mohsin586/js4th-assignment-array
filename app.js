a26111f0491570e58ee59dd286fcca59ffca4678
mohsin586 <143820469+mohsin586@users.noreply.github.com>
2023-09-19 13:52:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,179 @@
+
+// visualViewport.splice(  );
+
+
+// variable.slice();
+
+
+
+// var arr = ["nokia . ", "appple", "oppo"];
+
+// document.write(
+//     "<select>" +
+//     "<option>" + arr[1] + "</option>" +
+//     "<option>" + arr[2] + "</option>" +
+//     "<option>" + arr[1] + "</option>" +
+//     "<option>" + arr[2] + "</option>"
+
+
+
+
+//     + "</select>"
+// )
+
+// console.log(arr[1]);
+
+// var arr1 = new Array[ ]
+
+// var animal = ["dog", "cat", "lion", "snake", "rat"];
+// console.log["array" + animal];
+
+// var tr = +prompt("enter value");
+// var del = +prompt("enter del value");
+// var add = prompt("enter add value");
+// var add1 = prompt("enter addvalue");
+// var add2 = prompt("enter add value");
+
+// animal.splice(tr, del, add, add1);
+
+// console.log("updated" + animal);
+
+
+
+// for loop
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 0; i <= limit; i = i + 4) {
+
+//     console.log(i);
+
+// }
+
+
+
+
+
+// var i, arr = ["ali", true, 121, "done","jp",56];
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]+ "   index=   "+ i);
+
+
+// }
+
+
+//3
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 0; i <= limit; i = i + 1) {
+
+//     console.log(i);
+
+// }
+
+
+// 04
+
+//table of number
+
+// var a, b;
+
+// b = +prompt("enter the number , table you want");
+// var c = +prompt("enter length of table");
+// for (a = 1; a <= c; a++) {
+//     console.log(b + "  X  " + a + "  =  " + b * a);
+
+// }
+
+
+//05
+
+// var i, arr = ["apple", "banana", "mango", "straberry"];
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]+ "   index=   "+ i);
+
+// }
+
+//06
+
+//a
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 0; i <= limit; i = i + 1) {
+
+//     console.log(i);
+// }
+
+//b
+
+// var i;
+// for (i = 10; i >= 1; i = i - 1) {
+
+//     console.log(i);
+// }
+
+// c
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 0; i <= limit; i = i + 2) {
+
+//     console.log(i);
+
+// }
+
+// c
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 1; i <= limit; i = i + 2) {
+
+//     console.log(i);
+
+// }
+
+
+//d
+// var limit = +prompt("enter limit");
+// var i;
+// for (i = 0; i <= limit; i = i + 2) {
+
+//     console.log(i + "k");
+
+// }
+
+
+
+// var a = ["cake", "apple pie", "cookie", "chip", "patties"];
+
+// var input = prompt("what do want , abc bakers  ");
+
+// for (var i = 0; i < a.length; i++) {
+
+//     if (input == a[i]) {
+
+//         console.log("yes avail");
+//         break;
+//     }
+
+//     else {
+
+//         console.log("sorry");
+//         break;
+//     }
+
+//  }
+
+
+
+
+// var array = [5, 99, 9, 96, 32, 5, 87, 38];
+// var largest = 0;
+
+// for (var i=0; i < array.length; i++) {
+//     if (array[i] > largest) {
+//         largest = array[i];
+//     }
+// }
+
+// console.log(largest);
+
+ 
+
